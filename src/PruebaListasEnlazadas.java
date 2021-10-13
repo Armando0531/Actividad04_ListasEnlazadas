4d9f52488623b10c1a24d4364172a3f2ef40e276
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-10-13 13:47:55 -0500

    Se crea la clase Nodo

---
+++ src/PruebaListasEnlazadas.java
@@ -0,0 +1,37 @@
+
+class Nodo{
+	private int dato;
+	private Nodo nodoSiguiente;
+	
+	public Nodo(int dato) {
+		this.dato = dato;
+		nodoSiguiente = null;
+	}
+
+	public int getDato() {
+		return dato;
+	}
+
+	public void setDato(int dato) {
+		this.dato = dato;
+	}
+
+	public Nodo getNodoSiguiente() {
+		return nodoSiguiente;
+	}
+
+	public void setNodoSiguiente(Nodo nodoSiguiente) {
+		this.nodoSiguiente = nodoSiguiente;
+	}
+
+}
+
+
+public class PruebaListasEnlazadas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
