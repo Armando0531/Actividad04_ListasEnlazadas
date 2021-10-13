dbf6965a08287cc514ec0df79326a94eed48ece8
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-10-13 16:13:05 -0500

    Se agrego metodos para eliminar elementos, mostrar la cantidad de elementos y un menu para el usuario, se termina el programa

--- src/PruebaListasEnlazadas.java
+++ src/PruebaListasEnlazadas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.util.LinkedList;
 
 class Nodo{
 	private int dato;
@@ -22,6 +24,11 @@ class Nodo{
 
 	public void setNodoSiguiente(Nodo nodoSiguiente) {
 		this.nodoSiguiente = nodoSiguiente;
+	}
+	
+	@Override
+	public String toString() {
+		return "Nodo [dato=" + dato + ", nodoSiguiente=" + nodoSiguiente + "]";
 	}
 
 }
@@ -62,12 +69,85 @@ class ListaEnlazada{
 		}
 	}
 	
+	public void eliminarElementoInicial() {
+		if(listaVacia()) {
+			System.out.println("No se puede eliminar lista Vacia");
+		}else if(nodoInicio==nodoFin) {
+			nodoInicio=nodoFin=null;
+		}else {
+			Nodo nodoAuxiliar=nodoInicio;
+			System.out.println("Se elimino el dato: "+nodoAuxiliar.getDato());
+			nodoInicio=nodoInicio.getNodoSiguiente();
+			nodoAuxiliar=null;
+		}
+	}
+	
+	public void eliminarElementoFinal() {
+		if(listaVacia()) {
+			System.out.println("No se puede eliminar lista Vacia");
+		}else if(nodoInicio==nodoFin) {
+			nodoInicio=nodoFin=null;
+		}else {
+			Nodo nodoAuxiliar=nodoInicio;
+			while(nodoAuxiliar.getNodoSiguiente()!=nodoFin) {
+				nodoAuxiliar=nodoAuxiliar.getNodoSiguiente();
+			}
+			System.out.println("El dato eliminado es: "+nodoAuxiliar.getDato());
+			nodoFin=nodoAuxiliar;
+			nodoFin.setNodoSiguiente(null);
+		}
+	}
+	
+	public int eliminarDatoEspecifico(int dato) {
+		if(listaVacia()) {
+			return -1;
+		}else if (nodoInicio==nodoFin && nodoInicio.getDato()==dato){
+			Nodo nodoAux=nodoInicio;
+			nodoInicio=nodoFin=null;
+			return nodoAux.getDato();
+		}else {
+			Nodo nodoAnterior,nodoSig;
+			nodoAnterior=nodoInicio;
+			nodoSig=nodoAnterior.getNodoSiguiente();
+			while(nodoSig !=null && nodoSig.getDato()!=dato) {
+				nodoAnterior = nodoAnterior.getNodoSiguiente();
+				nodoSig=nodoSig.getNodoSiguiente();
+			}
+			if(nodoInicio==nodoSig) {
+				int x=0;
+				nodoInicio=nodoInicio.getNodoSiguiente();
+				x=nodoInicio.getDato();
+				return x;
+			}
+				if(nodoSig!=null && nodoSig.getDato()==dato ) {
+					int n=nodoSig.getDato();
+					nodoAnterior.setNodoSiguiente(nodoSig.getNodoSiguiente());
+					nodoSig=nodoSig.getNodoSiguiente();
+					return n;
+				}else {
+					return -2;
+				}
+		}
+		
+	}
+	
+	public void mostrarCantidadElementos() {
+		Nodo nodoActual=nodoInicio;
+		int cantidad=0;
+		while(nodoActual!=null) {
+			cantidad++;
+			nodoActual=nodoActual.getNodoSiguiente();
+		}
+		System.out.println("La lista tiene: "+cantidad);
+	}
+	
 	public void mostrarListaEnlazada() {
 		Nodo nodoActual = nodoInicio;
 		while(nodoActual!=null){
 			System.out.print("["+nodoActual.getDato()+"]-->");
 			nodoActual=nodoActual.getNodoSiguiente();
 		}
+		System.out.println();
 	}
 	
 }
@@ -76,23 +156,98 @@ class ListaEnlazada{
 public class PruebaListasEnlazadas {
 
 	public static void main(String[] args) {
-		ListaEnlazada le1 = new ListaEnlazada();
-		le1.agregarElementoAlInicio(7);
-		le1.agregarElementoAlInicio(1);
-		le1.agregarElementoAlInicio(2);
+		LinkedList<Integer> lc= new LinkedList<Integer>();
+		Scanner entrada=new Scanner(System.in);
+		int op=0;
+		ListaEnlazada lista=new ListaEnlazada();
+		do {
+		System.out.println("Elige una opcion:");
+		System.out.println("1- Verificar si la lista esta vacia");
+		System.out.println("2- Insertar elemento");
+		System.out.println("3- Eliminar elemento");
+		System.out.println("4- Cantidad de elementos");
+		System.out.println("5- Mostrar elementos");
+		System.out.println("6- Salir ");
+		op=entrada.nextInt();
+		switch (op) {
+		case 1:
+			System.out.println(lista.listaVacia() ?"La lista esta vacia":"La lista no esta vacia");
+			break;
+		case 2:
+			int dato=0;
+			String op2="";
+			do {
+				entrada.nextLine();
+				System.out.println("A) Insertar al inicio de la lista");
+				System.out.println("B) Insertar al Final de la lista");
+				System.out.println("C) Regresar al menu principal");
+				op2=entrada.nextLine().toUpperCase();
+				switch(op2) {
+				case "A":
+					System.out.print("Ingresa el dato: ");
+					dato=entrada.nextInt(); System.out.println();
+					lista.agregarElementoAlInicio(dato);
+					break;
+				case "B":
+					System.out.print("Ingresa el dato: ");
+					dato=entrada.nextInt(); System.out.println();
+					lista.agregarElementoAlFinal(dato);
+					break;
+				case "C":
+					System.out.println("Regresando al menu principal");
+					break;
+					default:
+						System.out.println("Elige una opcion disponible");
+						break;
+				}
+			}while(!(op2.equalsIgnoreCase("A") ||op2.equalsIgnoreCase("B") ||op2.equalsIgnoreCase("C") ));
+			break;
+		case 3:
+			do {
+				entrada.nextLine();
+				System.out.println("A) Eliminar al inicio de la lista");
+				System.out.println("B) Eliminar al Final de la lista");
+				System.out.println("C) Eliminar dato especifico");
+				op2=entrada.nextLine().toUpperCase();
+				switch(op2) {
+				case "A":
+					lista.eliminarElementoInicial();
+					break;
+				case "B":
+					lista.eliminarElementoInicial();
+					break;
+				case "C":
+					System.out.println("Ingresa el dato");
+					int datoEliminar=entrada.nextInt();
+					int num=lista.eliminarDatoEspecifico(datoEliminar);
+					if(num==-1) {
+						System.out.println("Lista vacia");
+					}else {
+						System.out.println(num==-2?" Dato no encontrado" :"Se elimino el dato "+num+" correctamente");
+					}
+					
+					break;
+					default:
+						System.out.println("Elige una opcion disponible");
+						break;
+				}
+			}while(!(op2.equalsIgnoreCase("A") ||op2.equalsIgnoreCase("B")||op2.equalsIgnoreCase("C")));
+			break;
+		case 4:
+			lista.mostrarCantidadElementos();
+			break;
+		case 5:
+			lista.mostrarListaEnlazada();
+			break;
+		case 6:
+			System.out.println("Programa Finalizado");
+			break;
+		default:
+			System.out.println("Error elige una opcion disponible en el menu");
+			break;
+		}
 		
-		System.out.println(le1);
-		
-		le1.mostrarListaEnlazada();
-		
-		System.out.println("\n----------------------------------");
-		
-		ListaEnlazada le2 = new ListaEnlazada();
-		le2.agregarElementoAlFinal(5);
-		le2.agregarElementoAlFinal(1);
-		le2.agregarElementoAlFinal(6);
-
-		le2.mostrarListaEnlazada();
+		}while(op!=6);
 	}
 
 }
